839851d655b795e22036f4a13ddd0d53d92282f5
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-03-20 16:27:39 +0000

    Declare some basic variables

--- src/com/javaeasily/demos/App.java
+++ src/com/javaeasily/demos/App.java
@@ -3,5 +3,8 @@ package com.javaeasily.demos;
 public class App {
     public static void main(String[] args) {
         System.out.println("Loan Calculator");
+        int loanAmount = 1000;
+        int numberOfYears = 5;
+        double interestRate = 1.5;
     }
 }
