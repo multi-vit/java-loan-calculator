11565945b269c661366a5f201067ddf0a281a7e2
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-03-22 08:58:50 +0000

    Work out 5 years of repayments to practise expressions with terrible DRY code

--- src/com/javaeasily/demos/App.java
+++ src/com/javaeasily/demos/App.java
@@ -3,8 +3,14 @@ package com.javaeasily.demos;
 public class App {
     public static void main(String[] args) {
         System.out.println("Loan Calculator");
-        int loanAmount = 1000;
+        int loanAmount = 100;
         int numberOfYears = 5;
-        double interestRate = 1.5;
+        double interestRate = 10.0;
+        double interestRateMultiplier = interestRate / 100 + 1;
+        double yearOne = loanAmount * interestRateMultiplier;
+        double yearTwo = yearOne * interestRateMultiplier;
+        double yearThree = yearTwo * interestRateMultiplier;
+        double yearFour = yearThree * interestRateMultiplier;
+        double yearFive= yearFour * interestRateMultiplier;
     }
 }
