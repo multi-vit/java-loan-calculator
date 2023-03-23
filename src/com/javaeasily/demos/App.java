35d15edff5d79b8339d21e6c86c6939e045513a3
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-03-23 12:22:18 +0000

    Complete part 5 - working wht conditionals

--- src/com/javaeasily/demos/App.java
+++ src/com/javaeasily/demos/App.java
@@ -7,20 +7,25 @@ public class App {
         int loanAmount = 100;
         int numberOfYears = 5;
         double interestRate = 10.0;
-        System.out.println("Calculating loan based on:");
-        System.out.println("Initial Amount:   £" + loanAmount);
-        System.out.println("Number of years:  " + numberOfYears);
-        System.out.println("Interest Rate:    " + interestRate + "%"
-        );
-        // Shouldn't use doubles for currency, should use BigDecimal, but this is just for beginner purposes.
-        double interestRateMultiplier = interestRate / 100 + 1;
-        double yearOne = loanAmount * interestRateMultiplier;
-        double yearTwo = yearOne * interestRateMultiplier;
-        double yearThree = yearTwo * interestRateMultiplier;
-        double yearFour = yearThree * interestRateMultiplier;
-        double yearFive= yearFour * interestRateMultiplier;
-        String yearFiveString = Double.toString(yearFive);
-        String totalAmountDue = yearFiveString.substring(0, yearFiveString.indexOf(".") + 3);
-        System.out.println("Total Amount Due: £" + totalAmountDue);
+        if (loanAmount > 0 && numberOfYears > 0 && interestRate > 0) {
+            System.out.println("Calculating loan based on:");
+            System.out.println("Initial Amount:   £" + loanAmount);
+            System.out.println("Number of years:  " + numberOfYears);
+            System.out.println("Interest Rate:    " + interestRate + "%"
+            );
+            // Shouldn't use doubles for currency, should use BigDecimal, but this is just for beginner purposes.
+            double interestRateMultiplier = interestRate / 100 + 1;
+            double yearOne = loanAmount * interestRateMultiplier;
+            double yearTwo = yearOne * interestRateMultiplier;
+            double yearThree = yearTwo * interestRateMultiplier;
+            double yearFour = yearThree * interestRateMultiplier;
+            double yearFive= yearFour * interestRateMultiplier;
+            String yearFiveString = Double.toString(yearFive);
+            String totalAmountDue = yearFiveString.substring(0, yearFiveString.indexOf(".") + 3);
+            System.out.println("Total Amount Due: £" + totalAmountDue);
+        } else {
+            System.out.println("Invalid values - cannot calculate repayment amount.");
+        }
+
     }
 }
